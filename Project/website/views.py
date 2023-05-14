9b83aeed686f3c3400bed4420e8eaaf4dd97f172
yongieghe <21521250@gmail.com>
2023-05-14 16:12:50 +0700

    A

---
+++ Project/website/views.py
@@ -0,0 +1,11 @@
+from flask import Blueprint, render_template, request, flash, jsonify
+from flask_login import login_required, current_user
+from . import db
+import json
+
+views = Blueprint('views', __name__)
+@views.route('/', methods=['GET', 'POST'])
+
+def home():
+    return "home.html"
+
