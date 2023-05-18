6451e31bc16c0b29498d7790ae16a8249d353b1a
yongieghe <21521250@gmail.com>
2023-05-18 20:38:16 +0700

    Vo duoc sign_up dau tien roi

--- Project/main.py
+++ Project/main.py
@@ -1,6 +1,11 @@
+from flask import Flask, render_template
 from website import create_app
 
 app = create_app()
-
+@app.route("/")
+def sign_up():
+    user = {"username": "Hong Nhung"}
+    return render_template("sign_up.html", user = user)
 if __name__ == '__main__':
     app.run(debug=True)
+
