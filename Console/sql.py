3a93f2460af9d98984411a12bb059984a12c29d9
Tuan Le <letuanvl03@gmail.com>
2023-06-20 13:13:13 +0700

    Convert SQLite3 to MongoDB

--- Console/sql.py
+++ Console/sql.py
@@ -1,15 +1,13 @@
-import sqlite3
+import pymongo
+from pymongo import MongoClient
 
-conn = sqlite3.connect('database.db')
-c = conn.cursor()
+client = MongoClient('https://ap-southeast-1.aws.data.mongodb-api.com/app/data-wwzqj/endpoint/data/v1/action/')  # Connect to MongoDB client
+db = client['Data']
+collection = db['user']
 
-c.execute(
-    """
-    select * from user
+data = collection.find()  # Modify the query as per your requirements
 
-    """
-)
-data = c.fetchall()
-print(data[0][1])
-conn.commit()
-conn.close()
+for document in data:
+    print(document)
+
+client.close()  # Close the MongoDB client connection
